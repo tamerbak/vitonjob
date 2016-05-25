dcf920252094c788622234768a2d7c5218105baa
amalrochd <amalrochd@gmail.com>
2016-05-25 14:06:23 +0100

    connexion data validation

--- app/providers/validation-data.service.ts
+++ app/providers/validation-data.service.ts
@@ -18,23 +18,11 @@ export class ValidationDataService {
 		* @description check if an email is valid
 		* @param id of the email component
 	*/
-	checkEmail(id) {
+	checkEmail(email) {
       var EMAIL_REGEXP = /^[_a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
-	  //console.log(id);
-	  //console.log(document.querySelector('#' + id));
-      //elm = angular.element(document.querySelector('#' + id));
-	  //var elm = DOM.query($('#' + id));
-      //console.log(elm);
-      var isMatchRegex = EMAIL_REGEXP.test(id);
-      console.log("Email : " + id);
-
-      if (isMatchRegex) {
-		this.dom.removeClass(this.dom.query("email"), 'has-warning').addClass(this.dom.query("email"), 'has-success');
-        //elm.parent().removeClass('has-warning').addClass('has-success');
-      }
-      else if (isMatchRegex == false || id == '') {
-        this.dom.removeClass(this.dom.query("email"), 'has-success').addClass(this.dom.query("email"), 'has-warning');
-        //elm.parent().removeClass('has-success').addClass('has-warning');
-      }
+      var isMatchRegex = EMAIL_REGEXP.test(email);
+      console.log("Email : " + email);
+	  console.log("is valid : " + isMatchRegex);
+	  return isMatchRegex;
     }
 }
