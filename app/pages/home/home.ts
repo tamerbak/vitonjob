6b4325a3e7cd8b543cf37045a035638cc55f1f7c
Jakjoud Abdeslam <jakjoud@gmail.com>
2016-05-26 08:33:11 +0200

    Correction of indenting

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -13,59 +13,59 @@ import {Events} from 'ionic-angular';
 
 
 @Page({
-    templateUrl: 'build/pages/home/home.html',
-    providers: [GlobalConfigs]
+	templateUrl: 'build/pages/home/home.html',
+	providers: [GlobalConfigs]
 })
 export class HomePage {
-    private projectName:string;
-    private themeColor:string;
-    private isEmployer:string;
-    private imageURL:string;
-    private searchPlaceHolder:string;
-    private projectTarget:string;
-    private highlightSentence:string;
-    private search : any;
-    private selectedItem : any;
-    private cnxBtnName : string;
+	private projectName:string;
+	private themeColor:string;
+	private isEmployer:string;
+	private imageURL:string;
+	private searchPlaceHolder:string;
+	private projectTarget:string;
+	private highlightSentence:string;
+	private search : any;
+	private selectedItem : any;
+	private cnxBtnName : string;
 	scQuery : string;
-    popinCrietria : boolean = false;
+	popinCrietria : boolean = false;
 	private isConnected : boolean;
-	
-	
-    static get parameters() {
-	return [[GlobalConfigs], [IonicApp], [NavController], [NavParams], [SearchService],[NetworkService], [Events]];
+
+
+	static get parameters() {
+		return [[GlobalConfigs], [IonicApp], [NavController], [NavParams], [SearchService],[NetworkService], [Events]];
 	}
-	
-    constructor(public globalConfig: GlobalConfigs,
-	private app: IonicApp,
-	private nav: NavController,
-	private navParams: NavParams,
-	private searchService: SearchService,
-	public networkService: NetworkService, 
-	public events: Events) {
-		
-        // Get target to determine configs
-        this.projectTarget = globalConfig.getProjectTarget();
+
+	constructor(public globalConfig: GlobalConfigs,
+				private app: IonicApp,
+				private nav: NavController,
+				private navParams: NavParams,
+				private searchService: SearchService,
+				public networkService: NetworkService,
+				public events: Events) {
+
+		// Get target to determine configs
+		this.projectTarget = globalConfig.getProjectTarget();
 		this.storage = new Storage(SqlStorage);
-		
-        //get name of the connexion btn
-        //this.cnxBtnName = globalConfig.getCnxBtnName();
-		
-        // get config of selected target
-        let config = Configs.setConfigs(this.projectTarget);
-		
-        //Initialize controller variables :
-        this.projectName = config.projectName;
-        this.themeColor = config.themeColor;
-        this.imageURL = config.imageURL;
-        this.highlightSentence = config.highlightSentence;
-        this.isEmployer = this.projectTarget == 'employer';
-        this.searchPlaceHolder = "Veuillez saisir votre recherche...";
-		
-        this.nav = nav;
-        // If we navigated to this page, we will have an item available as a nav param
-        this.selectedItem = navParams.get('item');
-        this.search = searchService;
+
+		//get name of the connexion btn
+		//this.cnxBtnName = globalConfig.getCnxBtnName();
+
+		// get config of selected target
+		let config = Configs.setConfigs(this.projectTarget);
+
+		//Initialize controller variables :
+		this.projectName = config.projectName;
+		this.themeColor = config.themeColor;
+		this.imageURL = config.imageURL;
+		this.highlightSentence = config.highlightSentence;
+		this.isEmployer = this.projectTarget == 'employer';
+		this.searchPlaceHolder = "Veuillez saisir votre recherche...";
+
+		this.nav = nav;
+		// If we navigated to this page, we will have an item available as a nav param
+		this.selectedItem = navParams.get('item');
+		this.search = searchService;
 		//verify if the user is already connected
 		this.storage.get("currentUser").then((value) => {
 			if(value){
@@ -77,19 +77,19 @@ export class HomePage {
 			}
 		});
 	}
-	
-    onFocus() {
-        if (this.projectTarget == 'employer')
-		this.searchPlaceHolder = "Ex : Je cherche un serveur débutant disponible demain sur Villepinte";
-        else
-		this.searchPlaceHolder = "Ex : Je cherche une offre d'emploi pour serveur débutant demain sur Villepinte";
+
+	onFocus() {
+		if (this.projectTarget == 'employer')
+			this.searchPlaceHolder = "Ex : Je cherche un serveur débutant disponible demain sur Villepinte";
+		else
+			this.searchPlaceHolder = "Ex : Je cherche une offre d'emploi pour serveur débutant demain sur Villepinte";
 	}
-	
-    onBlur() {
-        this.searchPlaceHolder = "Veuillez saisir votre recherche...";
+
+	onBlur() {
+		this.searchPlaceHolder = "Veuillez saisir votre recherche...";
 	}
-	
-    openLoginsPage() {
+
+	openLoginsPage() {
 		if(this.isConnected){
 			this.storage.set("currentUser", null);
 			this.cnxBtnName = "Se connecter / S'inscrire";
@@ -99,73 +99,73 @@ export class HomePage {
 			this.nav.push(LoginsPage);
 		}
 	}
-	
-    /**
-		* @author abdeslam jakjoud
-		* @description perform semantic search and pushes the results view
-	*/
-    doSemanticSearch(){
-        let loading = Loading.create({
-            content: ` 
+
+	/**
+	 * @author abdeslam jakjoud
+	 * @description perform semantic search and pushes the results view
+	 */
+	doSemanticSearch(){
+		let loading = Loading.create({
+			content: ` 
 			<div>
 			<img src='img/loading.gif' />
 			</div>
 			`,
-		spinner : 'hide'
-});
-this.nav.present(loading);
-console.log('Initiating search for '+this.scQuery);
-this.searchService.semanticSearch(this.scQuery, 0, this.projectTarget).then((data) => {
-	this.searchService.persistLastSearch(data);
-	loading.dismiss();
-	this.nav.push(SearchResultsPage);
-});
-}
+			spinner : 'hide'
+		});
+		this.nav.present(loading);
+		console.log('Initiating search for '+this.scQuery);
+		this.searchService.semanticSearch(this.scQuery, 0, this.projectTarget).then((data) => {
+			this.searchService.persistLastSearch(data);
+			loading.dismiss();
+			this.nav.push(SearchResultsPage);
+		});
+	}
 
-/**
-	* @author abdeslam jakjoud
-	* @description checking whether the user used the enter button to startup the semantic search
-	* @param e Key Up javascript event allowing us to access the keyboard used key
-*/
-checkForEnterKey(e){
-	if(e.code != "Enter")
-	return;
-	
-	this.doSemanticSearch();
-}
+	/**
+	 * @author abdeslam jakjoud
+	 * @description checking whether the user used the enter button to startup the semantic search
+	 * @param e Key Up javascript event allowing us to access the keyboard used key
+	 */
+	checkForEnterKey(e){
+		if(e.code != "Enter")
+			return;
 
-/**
-	* @description this method allows to render the multicriteria modal component
-*/
-showCriteriaModal(){
-	var dismissedModal = function(){
-		this.popinCrietria = false;
-	};
-	
-	if(this.popinCrietria)
-	return;
-	console.log('TEST');
-	let m = new Modal(SearchCriteriaPage);
-	m.onDismiss(dismissedModal.bind(this));
-	this.popinCrietria = true;
-	this.nav.present(m);
-}
+		this.doSemanticSearch();
+	}
 
-/**
-	* @description this method allows to render the guided search modal component
-*/
-showGuideModal(){
-	var dismissedModal = function(){
-		this.popinCrietria = false;
-	};
-	
-	if(this.popinCrietria)
-	return;
-	console.log('TEST');
-	let m = new Modal(SearchGuidePage);
-	m.onDismiss(dismissedModal.bind(this));
-	this.popinCrietria = true;
-	this.nav.present(m);
-}
+	/**
+	 * @description this method allows to render the multicriteria modal component
+	 */
+	showCriteriaModal(){
+		var dismissedModal = function(){
+			this.popinCrietria = false;
+		};
+
+		if(this.popinCrietria)
+			return;
+		console.log('TEST');
+		let m = new Modal(SearchCriteriaPage);
+		m.onDismiss(dismissedModal.bind(this));
+		this.popinCrietria = true;
+		this.nav.present(m);
+	}
+
+	/**
+	 * @description this method allows to render the guided search modal component
+	 */
+	showGuideModal(){
+		var dismissedModal = function(){
+			this.popinCrietria = false;
+		};
+
+		if(this.popinCrietria)
+			return;
+		console.log('TEST');
+		let m = new Modal(SearchGuidePage);
+		m.onDismiss(dismissedModal.bind(this));
+		this.popinCrietria = true;
+		this.nav.present(m);
+	}
 
 }
