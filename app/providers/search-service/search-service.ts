7aca39370135d170a6fe6c34efa50f00cb5ba39b
Jakjoud Abdeslam <jakjoud@gmail.com>
2016-06-29 09:24:50 +0200

    Adding details about offer in results cards

--- app/providers/search-service/search-service.ts
+++ app/providers/search-service/search-service.ts
@@ -33,7 +33,7 @@ export class SearchService {
 
     var payload = {
       'class' : 'fr.protogen.masterdata.model.CCallout',
-      id : 164,
+      id : 166,
       args : [
         {
           class : 'fr.protogen.masterdata.model.CCalloutArguments',
