60c4740343b5d10daef230486b894d3042c9edd3
Jakjoud Abdeslam <jakjoud@gmail.com>
2016-07-16 10:09:07 +0200

    medecine de travail and contract details

--- app/providers/medecine-service/medecine-service.ts
+++ app/providers/medecine-service/medecine-service.ts
@@ -34,7 +34,8 @@ export class MedecineService {
     }
 
     getMedecine(id){
-        let sql = "select pk_user_medecine_de_travail as id, libelle from user_medecine_de_travail where pk_user_medecine_de_travail in (select fk_user_medecine_de_travail from user_entreprise where pk_user_entreprise="+id+")";
+        let sql = "select pk_user_medecine_de_travail as id, libelle, adresse, cp.code as code_postal from user_medecine_de_travail, user_code_postal cp where user_medecine_de_travail.fk_user_code_postal=cp.pk_user_code_postal and pk_user_medecine_de_travail in (select fk_user_medecine_de_travail from user_entreprise where pk_user_entreprise="+id+")";
+        console.log(sql);
         return new Promise(resolve => {
             let headers = new Headers();
             headers.append("Content-Type", 'text/plain');
