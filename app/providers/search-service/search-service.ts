de6c35f02388340d44511bd2a978c84a42334833
Jakjoud Abdeslam <jakjoud@gmail.com>
2016-08-01 16:03:10 +0200

    Corrections diverses

--- app/providers/search-service/search-service.ts
+++ app/providers/search-service/search-service.ts
@@ -33,7 +33,7 @@ export class SearchService {
 
     var payload = {
       'class' : 'fr.protogen.masterdata.model.CCallout',
-      id : 196,
+      id : 204,
       args : [
         {
           class : 'fr.protogen.masterdata.model.CCalloutArguments',
