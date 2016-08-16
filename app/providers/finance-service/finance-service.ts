276831cd9ff06dd986f5450192f7c8b4c7af72b7
Jakjoud Abdeslam <jakjoud@gmail.com>
2016-08-16 17:00:05 +0200

    fixes #705 End of mission signature

--- app/providers/finance-service/finance-service.ts
+++ app/providers/finance-service/finance-service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/map';
 import {Http, Headers} from '@angular/http';
 import {Configs} from '../../configurations/configs';
+import {GlobalConfigs} from "../../configurations/globalConfigs";
 
 
 /*
@@ -13,8 +14,12 @@ import {Configs} from '../../configurations/configs';
 @Injectable()
 export class FinanceService {
     data: any;
-
-    constructor(private http: Http) {
+    invoiceId : number=0;
+    configuration:any;
+    projectTarget : any;
+    constructor(private http: Http, public gc:GlobalConfigs) {
+        this.projectTarget = gc.getProjectTarget();
+        this.configuration = Configs.setConfigs(this.projectTarget);
         this.data = null;
     }
 
@@ -92,6 +97,82 @@ export class FinanceService {
                     // we've got back the raw data, now generate the core schedule data
                     // and save the data for later reference
                     this.data = data;
+                    resolve(this.data);
+                });
+        });
+    }
+
+    loadInvoice(idContrat, id, rate){
+        let bean = {
+            'class' : 'com.vitonjob.callouts.finance.DocumentQuery',
+            idOffre : id,
+            idContrat : idContrat,
+            documentType : 'INVOICE',
+            appliedHourRate : rate
+        };
+        console.log(JSON.stringify(bean));
+        let encodedArg = btoa(JSON.stringify(bean));
+        var payload = {
+            'class': 'fr.protogen.masterdata.model.CCallout',
+            'id': 225,
+            'args': [
+                {
+                    'class': 'fr.protogen.masterdata.model.CCalloutArguments',
+                    label: 'Document query',
+                    value: encodedArg
+                }
+            ]
+        };
+
+        return new Promise(resolve => {
+            // We're using Angular Http provider to request the data,
+            // then on the response it'll map the JSON data to a parsed JS object.
+            // Next we process the data and resolve the promise with the new data.
+            let headers = new Headers();
+            headers.append("Content-Type", 'application/json');
+
+            this.http.post(Configs.calloutURL, JSON.stringify(payload), {headers:headers})
+                .map(res => res.json())
+                .subscribe(data => {
+                    //debugger;
+                    // we've got back the raw data, now generate the core schedule data
+                    // and save the data for later reference
+                    this.data = data;
+                    resolve(this.data);
+                });
+        });
+    }
+
+    loadInvoiceSignature(idInvoice){
+        let sql = "select * from user_facture_voj where pk_user_facture_voj="+idInvoice;
+        console.log(sql);
+        return new Promise(resolve => {
+            let headers = new Headers();
+            headers.append("Content-Type", 'text/plain');
+            this.http.post(this.configuration.sqlURL, sql, {headers: headers})
+                .map(res => res.json())
+                .subscribe(data => {
+                    this.data = data.data[0];
+                    resolve(this.data);
+                });
+        });
+    }
+
+    checkInvoice(idContrat){
+        let sql = "select * from user_facture_voj where fk_user_contrat="+idContrat;
+        console.log(sql);
+        return new Promise(resolve => {
+            let headers = new Headers();
+            headers.append("Content-Type", 'text/plain');
+            this.http.post(this.configuration.sqlURL, sql, {headers: headers})
+                .map(res => res.json())
+                .subscribe(data => {
+                    this.invoiceId = 0;
+                    this.data = null;
+                    if(data && data.data && data.data.length>0){
+                        this.data = data.data[0];
+                    }
+
                     resolve(this.data);
                 });
         });
