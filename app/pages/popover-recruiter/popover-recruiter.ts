44799007764c970a7f21378727ddc92093e4aaf6
amalrochd <amalrochd@gmail.com>
2016-08-22 15:57:58 +0200

    popover recruiter

---
+++ app/pages/popover-recruiter/popover-recruiter.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import {NavController, ViewController} from 'ionic-angular';
+
+@Component({
+	templateUrl: 'build/pages/popover-recruiter/popover-recruiter.html'
+})
+export class PopoverRecruiterPage {
+	ctrlView:any;
+	
+	constructor(private nav: NavController, ctrlView: ViewController) {
+		this.ctrlView = ctrlView;
+	}
+	
+	blockRecruiter() {
+		this.ctrlView.dismiss({option:1});
+	}
+	
+	deleteRecruiter(){
+		this.ctrlView.dismiss({option:2});
+	}
+}
