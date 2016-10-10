818ce7aeea8fe9944d14334313479e23f5198a48
tamerbak <tamer.elbakkali@gmail.com>
2016-10-10 18:02:38 +0200

    Back to TEST ENV

--- app/configurations/configs.ts
+++ app/configurations/configs.ts
@@ -124,24 +124,24 @@ class JobyerConfigs implements AbstractConfigs {
 
 export class Configs {
     /*
-    public static calloutURL:string = 'http://vitonjobv1.datqvvgppi.us-west-2.elasticbeanstalk.com/api/business';
-    public static sqlURL:string = 'http://vitonjobv1.datqvvgppi.us-west-2.elasticbeanstalk.com/api/sql';
-    public static yousignURL:string = 'http://vitonjobv1.datqvvgppi.us-west-2.elasticbeanstalk.com/api/business';
-    public static smsURL:string = 'http://vitonjobv1.datqvvgppi.us-west-2.elasticbeanstalk.com/api/envoisms';
-    public static emailURL:string = 'http://vitonjobv1.datqvvgppi.us-west-2.elasticbeanstalk.com/api/envoimail';
-    public static fssURL:string = 'http://vitonjobv1.datqvvgppi.us-west-2.elasticbeanstalk.com/api/fssjs';
+     public static calloutURL:string = 'http://vitonjobv1.datqvvgppi.us-west-2.elasticbeanstalk.com/api/business';
+     public static sqlURL:string = 'http://vitonjobv1.datqvvgppi.us-west-2.elasticbeanstalk.com/api/sql';
+     public static yousignURL:string = 'http://vitonjobv1.datqvvgppi.us-west-2.elasticbeanstalk.com/api/business';
+     public static smsURL:string = 'http://vitonjobv1.datqvvgppi.us-west-2.elasticbeanstalk.com/api/envoisms';
+     public static emailURL:string = 'http://vitonjobv1.datqvvgppi.us-west-2.elasticbeanstalk.com/api/envoimail';
+     public static fssURL:string = 'http://vitonjobv1.datqvvgppi.us-west-2.elasticbeanstalk.com/api/fssjs';
      */
 
 
-     public static calloutURL:string = 'https://app.vitonjob.com/api/business';
-     public static sqlURL:string = 'https://app.vitonjob.com/api/sql';
-     public static yousignURL:string = 'https://app.vitonjob.com/api/business';
-     public static smsURL:string = 'https://app.vitonjob.com/api/sms';
-     public static emailURL:string = 'https://app.vitonjob.com/api/email';
-     public static fssURL:string = 'https://app.vitonjob.com/api/fssjs';
+    public static calloutURL:string = 'https://app.vitonjob.com/api/business';
+    public static sqlURL:string = 'https://app.vitonjob.com/api/sql';
+    public static yousignURL:string = 'https://app.vitonjob.com/api/business';
+    public static smsURL:string = 'https://app.vitonjob.com/api/sms';
+    public static emailURL:string = 'https://app.vitonjob.com/api/email';
+    public static fssURL:string = 'https://app.vitonjob.com/api/fssjs';
 
 
-    public static getHttpJsonHeaders( ){
+    public static getHttpJsonHeaders() {
         let headers = new Headers();
         headers.append("Content-Type", 'application/json');
         headers.append("Authorization", 'Basic aGFkZXM6NWV0Y2Fy');
