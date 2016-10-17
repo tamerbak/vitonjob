e38c546921c1989434a2124e4680f277e260c105
Jakjoud Abdeslam <jakjoud@gmail.com>
2016-10-17 17:48:18 +0200

    Service for loading home screen data

---
+++ app/providers/home-service/home-service.ts
@@ -0,0 +1,115 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+/*
+ Generated class for the HomeService provider.
+
+ See https://angular.io/docs/ts/latest/guide/dependency-injection.html
+ for more info on providers and Angular 2 DI.
+ */
+@Injectable()
+export class HomeService {
+    data: any;
+
+    constructor(private http: Http) {
+        this.data = null;
+    }
+
+    /**
+     * Gets the appropriate home screen data for a preamptive selection of offers and tiers
+     * @param projectType
+     */
+    loadHomeData(projectType : string){
+        let query = {
+            "class":"com.vitonjob.model.Query",
+            dateReference:null,
+            resultCapacity:10,
+            resultCapacityOffers:10,
+            startIndex:0,
+            startIndexOffers:0,
+            type:projectType
+        };
+
+        console.log(JSON.stringify(query));
+        let encodedArg = btoa(JSON.stringify(query));
+        var payload = {
+            'class': 'fr.protogen.masterdata.model.CCallout',
+            'id': 308,
+            'args': [
+                {
+                    'class': 'fr.protogen.masterdata.model.CCalloutArguments',
+                    label: 'Document query',
+                    value: encodedArg
+                }
+            ]
+        };
+
+        return new Promise(resolve => {
+            // We're using Angular Http provider to request the data,
+            // then on the response it'll map the JSON data to a parsed JS object.
+            // Next we process the data and resolve the promise with the new data.
+            let headers = new Headers();
+            headers = Configs.getHttpJsonHeaders();
+
+            this.http.post(Configs.calloutURL, JSON.stringify(payload), {headers:headers})
+                .map(res => res.json())
+                .subscribe(data => {
+                    //debugger;
+                    // we've got back the raw data, now generate the core schedule data
+                    // and save the data for later reference
+                    this.data = data;
+                    resolve(this.data);
+                });
+        });
+
+    }
+
+    loadMore(projectType : string, offset : number, offersOffset){
+        let query = {
+            "class":"com.vitonjob.model.Query",
+            dateReference:null,
+            resultCapacity:10,
+            resultCapacityOffers:10,
+            startIndex:offset,
+            startIndexOffers:offersOffset,
+            type:projectType
+        };
+
+        console.log(JSON.stringify(query));
+        let encodedArg = btoa(JSON.stringify(query));
+        var payload = {
+            'class': 'fr.protogen.masterdata.model.CCallout',
+            'id': 308,
+            'args': [
+                {
+                    'class': 'fr.protogen.masterdata.model.CCalloutArguments',
+                    label: 'Document query',
+                    value: encodedArg
+                }
+            ]
+        };
+
+        return new Promise(resolve => {
+            // We're using Angular Http provider to request the data,
+            // then on the response it'll map the JSON data to a parsed JS object.
+            // Next we process the data and resolve the promise with the new data.
+            let headers = new Headers();
+            headers = Configs.getHttpJsonHeaders();
+
+            this.http.post(Configs.calloutURL, JSON.stringify(payload), {headers:headers})
+                .map(res => res.json())
+                .subscribe(data => {
+                    //debugger;
+                    // we've got back the raw data, now generate the core schedule data
+                    // and save the data for later reference
+                    this.data = data;
+                    resolve(this.data);
+                });
+        });
+
+    }
+
+
+}
+
