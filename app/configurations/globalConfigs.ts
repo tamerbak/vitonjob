e99e748f60d2ac6e1ea12499c13e4ebfbe394b1c
tamerbak <tamer.elbakkali@gmail.com>
2016-10-18 18:38:46 +0200

    TEST version 1.2.5/1.2.6

--- app/configurations/globalConfigs.ts
+++ app/configurations/globalConfigs.ts
@@ -21,7 +21,7 @@ export class GlobalConfigs {
     private missionOption : any;
 
     constructor() {
-        this.projectTarget = "jobyer";
+        this.projectTarget = "employer";
         this.cnxBtnName = "Se connecter / S'inscrire";
         this.thirdThemeColor = '#f4f4f4';
     }
