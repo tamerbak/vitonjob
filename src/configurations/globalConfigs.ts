acaeb897637ed712f1c4c543c2a71229c53fd0d9
amalrochd <amalrochd@gmail.com>
2017-03-14 11:08:31 +0000

    fixes #1960 ⁃ INS EMPLOYEUR lorsque je m'inscris via corporama et que les données sont importées, la civilité se superpose

--- src/configurations/globalConfigs.ts
+++ src/configurations/globalConfigs.ts
@@ -41,7 +41,7 @@ export class GlobalConfigs {
     static GA_APP_ID_JOB:string = 'UA-91039209-2';
 
     constructor() {
-        this.projectTarget = "jobyer"; // "jobyer" / "employer"
+        this.projectTarget = "employer"; // "jobyer" / "employer"
         this.cnxBtnName = "Se connecter / S'inscrire";
         this.thirdThemeColor = '#f4f4f4';
         this.googleAnalyticsID = this.projectTarget == "employer"?GlobalConfigs.GA_APP_ID_EMP:GlobalConfigs.GA_APP_ID_JOB;
